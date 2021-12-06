e5fcd8cff5710fb266d9deeaaecfdbfa902a2799
Diogo Pires <93797333+GreenMemory16@users.noreply.github.com>
2021-12-06 08:10:14 +0000

    Create solution.py

---
+++ 2021/Day 1/solution.py
@@ -0,0 +1,60 @@
+f = open("input.txt", "r")
+fishes = {}
+for i in range(9):
+    fishes[i] = 0
+
+starting_fish = [int(x) for x in f.readline().split(",")]
+for i in starting_fish:
+    fishes[i] += 1
+
+
+#####################################
+#               PART 1              #
+#####################################
+
+
+def start_simulation(days, fishes):
+
+    for i in range(days):
+        fishes = simulate_day(fishes)
+    return fishes
+
+
+def simulate_day(fishes):
+    n = 8
+    save = 0
+    save2 = fishes[8]
+
+    while n >= 0:
+        save2 = save
+        save = fishes[n]
+        fishes[n] = save2
+
+        if n == 0:
+            fishes[6] = fishes[6] + save
+            fishes[8] = save
+        n = n - 1
+
+    return fishes
+
+
+def counter(fishes):
+    count = 0
+    for i in range(9):
+        count = count + fishes[i]
+
+    return count
+
+
+fishes = start_simulation(80, fishes)
+
+print("Part 1: ", counter(fishes))
+
+
+#####################################
+#               PART 2              #
+#####################################
+
+fishes = start_simulation(176, fishes)
+
+print("Part 2: ", counter(fishes))
