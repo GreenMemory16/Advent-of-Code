da9ac6b5acf117ac076945bbc8ac5f891109b85a
Diogo Pires <93797333+GreenMemory16@users.noreply.github.com>
2022-12-04 09:46:37 +0000

    Create day4_1.js

---
+++ 2022/day4/day4_1.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+let count = 0;
+let lines = fs.readFileSync('./input.txt', 'utf-8').split('\n');
+let pairs = [];
+
+function isOverlaped(pair1, pair2) {
+    console.log(pair1 + "  " + pair2)
+    if ((parseInt(pair1[0]) >= parseInt(pair2[0]) && parseInt(pair1[0]) <= parseInt(pair2[1])) ||
+        (parseInt(pair2[0]) >= parseInt(pair1[0]) && parseInt(pair2[0]) <= parseInt(pair1[1]))) {
+        count ++;
+    }
+}
+
+for (let i=0; i < lines.length; i++) {
+    pairs = lines[i].split(',');
+    isOverlaped(pairs[0].split('-'), pairs[1].split('-'));
+}
+
+console.log(count);
