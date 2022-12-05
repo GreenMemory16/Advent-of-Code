71ca03215f60e168a96f7ccbf301eadc9f175b07
Diogo Pires <93797333+GreenMemory16@users.noreply.github.com>
2022-12-05 07:26:42 +0000

    Add files via upload

---
+++ 2022/day5/day5_2.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+var from = 0;
+var quantity = 0;
+var column = 0;
+
+let lines = fs.readFileSync('./input.txt', 'utf-8').split('\n');
+
+var crates =  [ ['Z', 'J', 'G'],
+                ['Q', 'L', 'R', 'P', 'W', 'F', 'V', 'C'],
+                ['F', 'P', 'M', 'C', 'L', 'G', 'R'],
+                ['L', 'F', 'B', 'W', 'P', 'H', 'M'],
+                ['G', 'C', 'F', 'S', 'V', 'Q'],
+                ['W', 'H', 'J', 'Z', 'M', 'Q', 'T', 'L'],
+                ['H', 'F', 'S', 'B', 'V'],
+                ['F', 'J', 'Z', 'S'],
+                ['M', 'C', 'D', 'P', 'F', 'H', 'B', 'T']];
+
+for (let i=0; i < lines.length; i++) {
+    lines[i] = lines[i].split(' ');
+    quantity = lines[i][1];
+    from = lines[i][3];
+    column = lines[i][5];
+    move(quantity, from, column);
+}
+
+function move(q, f, c) {
+    let index = crates[f-1].length - q; 
+    for (let i=0; i < q; i++) {
+        //console.log(index);
+        crates[c-1].push((crates[f-1].splice(index,1)));
+    }
+}
+
+function printCrates() {
+    let l = "";
+    for (let i=0; i < crates.length; i++) {
+        l = i+1 + ":  ";
+        for (let j=0; j < crates[i].length; j++) {
+            l += crates[i][j]; + ' ';
+        }
+        console.log(l);
+        l = ' ';
+    }
+}
+
+printCrates();
