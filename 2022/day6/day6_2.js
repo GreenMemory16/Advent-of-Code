26493c78cbf27bd94f8a21915a0069c256acd97f
Diogo Pires <93797333+GreenMemory16@users.noreply.github.com>
2022-12-06 08:58:29 +0000

    Add files via upload

---
+++ 2022/day6/day6_2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+let lines = fs.readFileSync('./input.txt', 'utf-8');
+var marker = [];
+
+for (let i=0; i < 14; i++) {
+    marker.push(lines[i]);
+}
+
+function detectMarker() {
+    for (let i = marker.length; i < lines.length; i++) {
+        if (!findDuplicates(marker)) {
+            return i;
+        }
+        marker.shift();
+        marker.push(lines[i]);
+    }
+}
+
+function findDuplicates(arr) {
+    return new Set(arr).size !== arr.length;
+}
+
+console.log(detectMarker());
+
